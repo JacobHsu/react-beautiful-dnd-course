5605e47a1b81a5424be461aa9ef9cdb7bc70afe9
jacobhsu <jacobhsu@jacobhsuMacBook-Pro.local>
2024-04-23 17:09:36 +0800

    Create and Style a List of Data

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import React, { useState } from 'react';
 import initialData from './initial-data';
 import Column from './column';
 import '@atlaskit/css-reset';
+import { DragDropContext } from 'react-beautiful-dnd';
+
 
 function App() {
   const [state, setState] = useState(initialData);
-  return state.columnOrder.map(columnId => {
-    const column = state.columns[columnId];
-    const tasks = column.taskIds.map(taskId => state.tasks[taskId]);
+  
+  const onDragEnd = result => {
+    // TODO: reorder our column
+  };
 
-    return <Column key={column.id} column={column} tasks={tasks} />;
-  });
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      {state.columnOrder.map(columnId => {
+        const column = state.columns[columnId];
+        const tasks = column.taskIds.map(taskId => state.tasks[taskId]);
+
+        return <Column key={column.id} column={column} tasks={tasks} />;
+      })}
+    </DragDropContext>
+  )
 }
 
 export default App;
